f08a9ca18f35cd6f0f127e68a8b67cb6e32795c0
salty-max <max@jellycat.fr>
2022-04-10 00:04:37 +0200

    add new guests

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,25 @@
 use std::io::stdin;
 
+#[derive(Debug)]
+struct Guest {
+    name: String,
+    greeting: String
+}
+
+impl Guest {
+    fn new(name: &str, greeting: &str) -> Self {
+        Self {
+            name: name.to_lowercase(),
+            greeting: greeting.to_string()
+        }
+    }
+
+
+    fn greet(&self) {
+        println!("{}", self.greeting);
+    }
+}
+
 fn what_is_your_name() -> String {
     let mut name = String::new();
 
@@ -9,23 +29,31 @@ fn what_is_your_name() -> String {
 }
 
 fn main() {
-    let guest_list = ["max", "lucile", "angus"];
-
+    let mut guest_list = vec![
+        Guest::new("max", "Hello Max, enjoy your treehouse."),
+        Guest::new("lucile", "Hi Lucile, your coke is in the fridge."),
+        Guest::new("angus", "Hey little Angus, always smiling I see.")
+    ];
+    loop {
     println!("Hello, what's your name?");
 
     let name = what_is_your_name();
 
-    let mut allow_them_in = false;
+    let known_guest = &guest_list.iter().find(|g| g.name == name);
 
-    for guest in &guest_list {
-        if guest == &name {
-            allow_them_in = true
-        }
+    match known_guest {
+        Some(guest) => guest.greet(),
+        None => {
+            if name.is_empty() {
+                break;
+            } else {
+                println!("{} is not on the guest list", name);
+                guest_list.push(Guest::new(&name, "New friend"));
+            }
+        },
+    }
     }
 
-    if allow_them_in {
-        println!("Welcome to the Treehouse, {}", &name);
-    } else {
-        println!("GTFO!")
-    }
+    println!("Final list of guests:");
+    println!("{:#?}", guest_list);
 }

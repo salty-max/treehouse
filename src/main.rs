74b3d9fbfbee3513f4ee8aa143fb1a672388f9aa
salty-max <max@jellycat.fr>
2022-04-10 00:35:44 +0200

    adds a real bouncer at the front

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,51 @@
 use std::io::stdin;
 
 #[derive(Debug)]
+enum GuestAction {
+    Accept,
+    AcceptWithNote { note: String },
+    Refuse,
+    Probation,
+}
+
+#[derive(Debug)]
 struct Guest {
     name: String,
-    greeting: String
+    action: GuestAction,
+    age: i8,
 }
 
 impl Guest {
-    fn new(name: &str, greeting: &str) -> Self {
+    fn new(name: &str, action: GuestAction, age: i8) -> Self {
         Self {
             name: name.to_lowercase(),
-            greeting: greeting.to_string()
+            action,
+            age,
         }
     }
 
-
     fn greet(&self) {
-        println!("{}", self.greeting);
+        match &self.action {
+            GuestAction::Accept => println!("Welcome to the treehouse, {}", capitalize(&self.name)),
+            GuestAction::AcceptWithNote { note } => {
+                println!(
+                    "Welcome to the treehouse, {} ({})",
+                    capitalize(&self.name),
+                    note
+                );
+                if self.age < 18 {
+                    println!("No alcohol!");
+                }
+            }
+            GuestAction::Probation => {
+                println!("{} is now a probationary member", capitalize(&self.name))
+            }
+            GuestAction::Refuse => println!("Do not allow {} in!", capitalize(&self.name)),
+        }
     }
 }
 
-fn what_is_your_name() -> String {
+fn ask_name() -> String {
     let mut name = String::new();
 
     stdin().read_line(&mut name).expect("Failed to read line");
@@ -28,30 +53,57 @@ fn what_is_your_name() -> String {
     name.trim().to_lowercase()
 }
 
+fn ask_age() -> i8 {
+    let mut age = String::new();
+    stdin().read_line(&mut age).expect("Failed to read line");
+
+    age.trim().parse::<i8>().expect("Failed to parse string")
+}
+
+pub fn capitalize(s: &str) -> String {
+    let mut c = s.chars();
+    match c.next() {
+        None => String::new(),
+        Some(f) => f.to_uppercase().collect::<String>() + c.as_str(),
+    }
+}
+
 fn main() {
     let mut guest_list = vec![
-        Guest::new("max", "Hello Max, enjoy your treehouse."),
-        Guest::new("lucile", "Hi Lucile, your coke is in the fridge."),
-        Guest::new("angus", "Hey little Angus, always smiling I see.")
+        Guest::new("max", GuestAction::Accept, 31),
+        Guest::new(
+            "lucile",
+            GuestAction::AcceptWithNote {
+                note: "Hide chocolate".to_string(),
+            },
+            35,
+        ),
+        Guest::new("angus", GuestAction::Accept, 0),
+        Guest::new("justine", GuestAction::Refuse, 29),
     ];
     loop {
-    println!("Hello, what's your name?");
+        println!("Hello, what's your name?");
 
-    let name = what_is_your_name();
+        let name = ask_name();
 
-    let known_guest = &guest_list.iter().find(|g| g.name == name);
+        let known_guest = &guest_list.iter().find(|g| g.name == name);
 
-    match known_guest {
-        Some(guest) => guest.greet(),
-        None => {
-            if name.is_empty() {
-                break;
-            } else {
-                println!("{} is not on the guest list", name);
-                guest_list.push(Guest::new(&name, "New friend"));
+        match known_guest {
+            Some(guest) => guest.greet(),
+            None => {
+                if name.is_empty() {
+                    break;
+                } else {
+                    println!("Hmm, you are not on the guest list...");
+                    println!("How old are you?");
+
+                    let age = ask_age();
+                    let new_guest = Guest::new(&name, GuestAction::Probation, age);
+                    new_guest.greet();
+                    guest_list.push(new_guest);
+                }
             }
-        },
-    }
+        }
     }
 
     println!("Final list of guests:");

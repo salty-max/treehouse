3b04360e457f9c472086663ff79ef4573d659212
salty-max <max@jellycat.fr>
2022-04-09 23:21:54 +0200

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,36 @@
+use std::io::stdin;
+
+struct Guest {
+    name: String,
+    greeting: String,
+}
+
+fn what_is_your_name() -> String {
+    let mut name = String::new();
+
+    stdin().read_line(&mut name).expect("Failed to read line");
+
+    name.trim().to_lowercase()
+}
+
+fn main() {
+    let guest_list = ["max", "lucile", "angus"];
+
+    println!("Hello, what's your name?");
+
+    let name = what_is_your_name();
+
+    let mut allow_them_in = false;
+
+    for guest in &guest_list {
+        if guest == &name {
+            allow_them_in = true
+        }
+    }
+
+    if allow_them_in {
+        println!("Welcome to the Treehouse, {}", &name);
+    } else {
+        println!("GTFO!")
+    }
+}
